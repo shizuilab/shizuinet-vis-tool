0b50961593ea24f852bb4454ea36e9e35fcbf7f3
sz0xff <tatsuya.suzuki.jp@gmail.com>
2023-04-01 15:49:04 +0900

    change something

--- script.js
+++ script.js
@@ -5,11 +5,9 @@ let EPOCH;
 let XYM_ID;
 let NODE_URL;
 let NET_TYPE;
-
 let repo;
 let accountRepo;
 let txRepo;
-
 let edgesArray = [];
 let nodesArray = [];
 
@@ -22,8 +20,8 @@ async function makeGraph(_nodesArray,_edgesArray){
     let edgesArray =_edgesArray;
     let nodesArray =_nodesArray;
 
-    console.log(edgesArray);
-    console.log(nodesArray);
+    // console.log(edgesArray);
+    // console.log(nodesArray);
 
 
     var options = {};
@@ -38,10 +36,14 @@ async function makeGraph(_nodesArray,_edgesArray){
         edges: edges
     };
     var options = {
-        // physics: false, // 物理シミュレーション
+        // tooltipDelay: 300,
+        interaction:{
+            hover:true
+        },
+        //physics: false, // 物理シミュレーション
         nodes: {
             shape: 'box', 
-            size: 30,
+            size: 10,
             font: {
                 color: 'black', 
             },
@@ -55,7 +57,6 @@ async function makeGraph(_nodesArray,_edgesArray){
             myGroup: {color:{background:'#ebeb4b'},borderWidth:3},
             otherGroup: {color:{background:'skyBlue'}, borderWidth:1}
         }
-        // ,interaction:{hover:true}
 
     };
     var network = new vis.Network(container, data, options);
@@ -64,21 +65,33 @@ async function makeGraph(_nodesArray,_edgesArray){
         if (params.nodes.length == 1) {
           var nodeId = params.nodes[0];
           var node = nodes.get(nodeId);
-          console.log(node.label + 'is clicked');
-          console.log(nodeId + 'is clicked');
+        //   console.log(node.label + 'is clicked');
+        //   console.log(nodeId + 'is clicked');
           nodeClick(nodeId);
         }
     });
+    network.on("hover",function(params){
+        if (params.nodes.length == 1) {
+            var nodeId = params.nodes[0];
+            var node = nodes.get(nodeId);
+            // console.log(node.label + ' hover');
+            // console.log(nodeId + ' hover');
+          }
+    });
+    container.style.cursor = "grab";
 }
 
 
-
 //TxlistからTxDetail取得
-async function getTxlistDetail(_Txlist){
+async function getTxlistDetail(_MyAddress,_Txlist){
 
+    let MyAddress = _MyAddress;
+
+    let MyRawAddress = MyAddress.address;
+    // console.log(MyRawAddress);
     let Txlist = _Txlist;
     let txes = Txlist.data;
-    console.log(txes.length);
+    // console.log(txes.length);
 
     let c =0;
     for (let tx of txes){
@@ -88,17 +101,61 @@ async function getTxlistDetail(_Txlist){
             case 16724: //Transfer
                 //JSON追加
                 console.log("trtx");
-                edgesArray.push({timestamp:tx.transactionInfo.timestamp,type:"sg-tr-tx",from: tx.signer.address.address, to: tx.recipientAddress.address,hash:tx.transactionInfo.hash});
+                console.log(tx);
+                edgesArray.push({
+                    timestamp:tx.transactionInfo.timestamp,
+                    type:"sg-tr-tx",
+                    from: tx.signer.address.address,
+                    to: tx.recipientAddress.address,
+                    hash:tx.transactionInfo.hash,
+                    message:tx.message.payload,
+                    mosaic:tx.mosaics,
+                    title:  "type: sg-tr-rx" + "<br>" + 
+                            "from: " + tx.signer.address.address  + "<br />" + 
+                            "to: " + tx.recipientAddress.address +"<br />" +
+                            "hash: " + tx.transactionInfo.hash +"<br />" +
+                            "message : " + tx.message.payload
+                        });
                 console.log("added"+c);
                 break;
             case 16705: //Aggregate
                 console.log("agtx");
+                edgesArray.push({
+                    timestamp:tx.transactionInfo.timestamp,
+                    type:"ag-tx",
+                    from: MyRawAddress,
+                    to: tx.signer.address.address,
+                    hash:tx.transactionInfo.hash,
+                    message:'none',
+                    mosaic:'none',
+                    title:  "type: ag-tx" + "<br>" + 
+                            "from: " + MyRawAddress  + "<br />" + 
+                            "to: " + tx.signer.address.address +"<br />" +
+                            "hash: " + tx.transactionInfo.hash +"<br />" +
+                            "message : " + "none"
+                });
+
                 txInfo = await txRepo.getTransaction(tx.transactionInfo.hash,sym.TransactionGroup.Confirmed).toPromise();
                 console.log(txInfo);
+
+
                 if(txInfo.type == 16724){ //Aggregate の 一つだけTransfer
                     console.log("ag-sg-tr");
                     //JSON追加
-                    edgesArray.push({timestamp:txInfo.transactionInfo.timestamp,type:"ag-sg-tr",from: txInfo.signer.address.address, to: txInfo.recipientAddress.address, hash:txInfo.transactionInfo.hash});
+                    edgesArray.push({
+                        timestamp:txInfo.transactionInfo.timestamp,
+                        type:"ag-sg-tr",
+                        from: txInfo.signer.address.address,
+                        to: txInfo.recipientAddress.address,
+                        hash:txInfo.transactionInfo.hash,
+                        message:tx.message.payload,
+                        mosaic:tx.mosaics,
+                        title:  "type: ag-sg-tr" + "<br>" + 
+                        "from: " + txInfo.signer.address.address  + "<br />" + 
+                        "to: " + txInfo.recipientAddress.address +"<br />" +
+                        "hash: " + txInfo.transactionInfo.hash +"<br />" +
+                        "message : " + tx.message.payload
+                });
                     console.log("added"+c);
                 }else{ //Aggregate の 複数Transfer
                     const inners = txInfo.innerTransactions;
@@ -108,7 +165,20 @@ async function getTxlistDetail(_Txlist){
                             //JSON追加
                             try{
                                 console.log("ag-ml-tr");
-                                edgesArray.push({timestamp:inner.transactionInfo.timestamp,type:"ag-ml-tr",from: inner.signer.address.address, to: inner.recipientAddress.address,hash:inner.transactionInfo.aggregateHash});
+                                edgesArray.push({
+                                    timestamp:inner.transactionInfo.timestamp,
+                                    type:"ag-ml-tr",
+                                    from: inner.signer.address.address,
+                                    to: inner.recipientAddress.address,
+                                    hash:inner.transactionInfo.aggregateHash,
+                                    message:inner.message.payload,
+                                    mosaic:inner.mosaics,
+                                    title:  "type: ag-ml-tr" + "<br>" + 
+                                    "from: " + inner.signer.address.address  + "<br />" + 
+                                    "to: " + inner.recipientAddress.address +"<br />" +
+                                    "hash: " + inner.transactionInfo.aggregateHash +"<br />" +
+                                    "message : " + inner.message.payload
+                                });
                             }catch(e){
                                 console.log("error");
                                 console.log(e);
@@ -122,6 +192,7 @@ async function getTxlistDetail(_Txlist){
         c = c+1;
 
     }
+    // console.table(edgesArray);
     return edgesArray;
 }
 
@@ -136,7 +207,7 @@ async function getTxlist(_MyAddress){
           address:MyAddress
         }
     ).toPromise();
-    return result;
+    return result;    
 }
 
 
@@ -152,15 +223,11 @@ async function getAccountInfo(_address){
 
 async function searchAddress(_RawAddress){
     let RawAddress =_RawAddress;
-    // let edgesArray = [];
-    // let nodesArray = [];
-
-    console.log(edgesArray);
     //初期化
     edgesArray.splice(0);
     nodesArray.splice(0);
     
-    console.log(RawAddress);
+    // console.log(RawAddress);
 
     switch(RawAddress.substr(0,1)){
         case "N":
@@ -194,67 +261,81 @@ async function searchAddress(_RawAddress){
     txRepo = repo.createTransactionRepository();
     
 
-    console.log(NODE_URL);
+    // console.log(NODE_URL);
 
     //RawAddressからMyAddress取得
     const MyAddress = sym.Address.createFromRawAddress(
         RawAddress
       );
-      console.log("MyAddress");
-      console.log(MyAddress); 
 
     //MyAddressからAccountInfo取得
     let AccountInfo = await getAccountInfo(MyAddress);
-    console.log("AccountInfo");
-    console.log(AccountInfo);    
 
     //AccountInfoからTxlist取得
     let Txlist = await getTxlist(MyAddress);
-    console.log("Txlist");
-    console.log(Txlist);
-
+ 
     //TxlistからTxDetail取得
-    let TxDetail = await getTxlistDetail(Txlist);
-    console.log("TxDetail");
-    console.log(TxDetail);
+    let TxDetail = await getTxlistDetail(MyAddress,Txlist,);
+     console.table(TxDetail);
+
 
     //nodesArray作成
+
+    // for (let e of TxDetail){
+    //     if(e.from === RawAddress && e.to === RawAddress ){
+    //         nodesArray.push({id:e.from,label:e.substr(0, 4)+"..."+e.slice(-4),group:"me"});
+    //         nodesArray.push({id:to.from,label:e.substr(0, 4)+"..."+e.slice(-4),group:"me"});
+    //     }else if(e.from === RawAddress && e.to !== RawAddress ){
+    //         nodesArray.push({id:e.from,label:e.substr(0, 4)+"..."+e.slice(-4),group:"me"});
+    //         nodesArray.push({id:to.from,label:e.substr(0, 4)+"..."+e.slice(-4),group:"other"});
+    //     }else if(e.from !== RawAddress && e.to === RawAddress ){
+    //         nodesArray.push({id:e.from,label:e.substr(0, 4)+"..."+e.slice(-4),group:"other"});
+    //         nodesArray.push({id:to.from,label:e.substr(0, 4)+"..."+e.slice(-4),group:"me"});
+    //     }else if(e.from !== RawAddress && e.to !== RawAddress ){
+    //         nodesArray.push({id:e.from,label:e.substr(0, 4)+"..."+e.slice(-4),group:"other"});
+    //         nodesArray.push({id:to.from,label:e.substr(0, 4)+"..."+e.slice(-4),group:"other"});
+    //     }
+    // }
+
+
     const fromList = TxDetail.map((obj) => obj.from);
     const toList = TxDetail.map((obj) => obj.to);
     let list = fromList.concat(toList);
     let list2 = [...new Set(list)];
     for (let e of list2){
         if(e === RawAddress){
-            nodesArray.push({id:e,label:e.substr(0, 4)+"..."+e.slice(-4),group:"myGroup"});
+            nodesArray.push({id:e,label:e.substr(0, 4)+"..."+e.slice(-4),group:"myGroup",title:"address: " + e});
             console.log("myGroup");
 
         }else{
-            nodesArray.push({id:e,label:e.substr(0, 4)+"..."+e.slice(-4),group:"otherGroup"});
+            nodesArray.push({id:e,label:e.substr(0, 4)+"..."+e.slice(-4),group:"otherGroup",title:"address: " + e});
             console.log("otherGroup");
 
         }
     }
     // console.log(nodesArray);
-    console.table(nodesArray);
+    // console.table(nodesArray);
 
 
     //edgesArray作成（TxDetailそのもの）
     edgesArray = TxDetail;
     // console.log(edgesArray);
-    console.table(edgesArray);
+    // console.table(edgesArray);
 
     //グラフ作成
     await makeGraph(nodesArray,edgesArray);
 
 
+
+}
+async function test(){
+    console.log("test");
 }
 
 async function searchClick(){
     console.log("clicked!");
     let element = document.getElementById('myAddress');
-    console.log(element.value);
     document.getElementById('network').innerText="under investigation...";
-
     await searchAddress(element.value);
 }
 
